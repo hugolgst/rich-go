4ece48be7b9c41fd22995f0496096bbf5c86e888
AnanaGame <gameanana@gmail.com>
2018-06-01 22:13:00 +0200

    Initial commit

---
+++ rich-go/ipc.go
@@ -0,0 +1,18 @@
+package rich_go
+
+import "os"
+
+// Return the current IPC socket's path
+func GetIpcPath() string {
+	variablesnames := []string{"XDG_RUNTIME_DIR", "TMPDIR", "TMP", "TEMP"}
+
+	for _, variablename := range variablesnames {
+		path, exists := os.LookupEnv(variablename)
+
+		if exists {
+			return path
+		}
+	}
+
+	return "/tmp"
+}
