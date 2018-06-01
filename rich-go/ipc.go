c6ef4a040d8d8b5d16f505238bdac91ca081b0fe
AnanaGame <gameanana@gmail.com>
2018-06-01 22:28:17 +0200

    Create the OpenSocket() method and send a payload

--- rich-go/ipc.go
+++ rich-go/ipc.go
@@ -1,6 +1,14 @@
 package rich_go
 
-import "os"
+import (
+	"os"
+	"net"
+	"fmt"
+	"encoding/binary"
+	"bytes"
+)
+
+var socket net.Conn
 
 // Return the current IPC socket's path
 func GetIpcPath() string {
@@ -15,4 +23,53 @@ func GetIpcPath() string {
 	}
 
 	return "/tmp"
+}
+
+// Open the discord-ipc-0 unix socket
+func OpenSocket() {
+	sock, err := net.Dial("unix", GetIpcPath() + "discord-ipc-0")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Listening to discord-ipc-0")
+
+	socket = sock
+}
+
+// Read the socket response
+func Read() string {
+	buf := make([]byte, 512)
+	payloadlength, err := socket.Read(buf)
+	if err != nil {
+		fmt.Println("Nothing to read")
+	}
+
+	buffer := new(bytes.Buffer)
+	for i := 8; i < payloadlength; i++ {
+		buffer.WriteByte(buf[i])
+	}
+
+	return string(buffer.Bytes())
+}
+
+// Send opcode and payload to the unix socket
+func Send(opcode int, payload string) string {
+	buf := new(bytes.Buffer)
+
+	err := binary.Write(buf, binary.LittleEndian, int32(opcode))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = binary.Write(buf, binary.LittleEndian, int32(len(payload)))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	buf.Write([]byte(payload))
+
+	socket.Write(buf.Bytes())
+
+	return Read()
 }
