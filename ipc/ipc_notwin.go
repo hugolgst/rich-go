ba62d8369611b8d02eccb0a5ff9fa2b91f9c2456
Stepan Rabotkin <epicstyt@gmail.com>
2021-04-10 14:48:56 +0300

    Fix IPC for unix v2

--- ipc/ipc_notwin.go
+++ ipc/ipc_notwin.go
@@ -9,7 +9,7 @@ import (
 
 // OpenSocket opens the discord-ipc-0 unix socket
 func (ipc *IPC) openSocket() error {
-	sock, err := net.DialTimeout("unix", ipc.GetIpcPath()+"/discord-ipc-0", time.Second*2)
+	sock, err := net.DialTimeout("unix", ipc.getIpcPath()+"/discord-ipc-0", time.Second*2)
 	if err != nil {
 		return err
 	}
